45003b1f0e7368929706018c6cae271672f484fc
chosunme96 <31688874+chosunme96@users.noreply.github.com>
2017-12-11 01:19:44 +0900

    8

--- PIM_final.js
+++ PIM_final.js
@@ -9,7 +9,7 @@ function check(){
   var id=document.getElementById("userid").value;
   var password=document.getElementById("userpswrd").value;
   if(id=='testid' && password='testpswrd'){
-    window.open("PIM_final.html");
+    window.open("createMoneyBook.html");
   }else{
     alert("Error Password or Username");
   }
@@ -20,3 +20,30 @@ function createMoneyBook(){
 
 
 }
+
+
+// Write JavaScript here // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
