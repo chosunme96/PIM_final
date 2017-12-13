9fd24cb1a4093ce11d1d4ba5829502d6469fc396
chosunme96 <31688874+chosunme96@users.noreply.github.com>
2017-12-13 10:58:37 +0900

    17

--- PIM_final.js
+++ PIM_final.js
@@ -5,10 +5,19 @@ var user(id, pswrd){
   this.pswrd=pswrd;
 }
 
+var mb=[];
+function moneyBook(name, description, date, color){
+  this.name=name;
+  this.description=description;
+  this.date=date;
+  this.color=color;
+}
+
 function check(){
   var id=document.getElementById("userid").value;
   var password=document.getElementById("userpswrd").value;
-  if(id=='testid' && password='testpswrd'){
+  if(id=='testid' && password=='testpswrd'){
+    document.getElementById("test").innerHTML="what";
     window.open("createMoneyBook.html");
   }else{
     alert("Error Password or Username");
@@ -16,13 +25,24 @@ function check(){
 }
 
 function createMoneyBook(){
+  var name=document.getElementById("mbname").value;
+  var description=document.getElementById("mbdescription").value;
+  var date=document.getElementById("mbdate").value;
+  var color=document.getElementById("mbcolor").value;
+  mb.push(new moneyBook(name, description, date, color));
+}
 
-
-
+function changestatus(){
+  var status=document.getElementsByClassName("earning");
+  if(status.value=="수입") status.value="지출";
+  else status.value="수입";
 }
 
 
-// Write JavaScript here // Get the modal
+
+
+// Write JavaScript here
+// Get the modal
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
