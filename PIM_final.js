010120f9b10b5e72dd38836988daeae49df713b8
chosunme96 <31688874+chosunme96@users.noreply.github.com>
2017-12-17 15:43:24 +0900

    24

--- PIM_final.js
+++ PIM_final.js
@@ -45,6 +45,93 @@ function changestatus(){
 }
 
 
+$(document).ready(function(){
+  $(".memobutton").click(function(){
+    if($("#memobox").is(":visible")){
+      $("#memobox").slideUp();
+    }else{
+      $("#memobox").slideDown();
+    }
+  });
+});
+
+
+
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").insertBefore(li,document.getElementById("myUL").firstChild);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+}
+
+function setMemoType(memot){
+  if(memot.id=="normalmemo"){
+    document.getElementById("normalmemo").checked=true;
+    document.getElementById("checklist").checked=false;
+    document.getElementById("memotype").innerHTML="메모";
+    document.getElementById("myUL").style.display="none";
+    document.getElementById("checklistpart").style.display="none";
+    document.getElementById("memopart").style.display="block";
+  }else if(memot.id=="checklist"){
+    document.getElementById("checklist").checked=true;
+    document.getElementById("normalmemo").checked=false;
+    document.getElementById("memotype").innerHTML="체크 리스트 ";
+    document.getElementById("myUL").style.display="block";
+    document.getElementById("checklistpart").style.display="block";
+    document.getElementById("memopart").style.display="none";
+  }
+
+}
+
 
 /*
 // Write JavaScript here
@@ -59,18 +146,18 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
 btn.onclick = function() {
-    modal.style.display = "block";
+modal.style.display = "block";
 };
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = "none";
+modal.style.display = "none";
 };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+if (event.target == modal) {
+modal.style.display = "none";
+}
 };
 */
